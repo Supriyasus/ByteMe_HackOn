b281a6ebf56ff61c9a42ff4abd8e2cafbead44c5
Vanshika Khanna <138656686+vanshikak04@users.noreply.github.com>
2025-06-23 00:18:33 +0530

    Update db.py

--- backend/database/db.py
+++ backend/database/db.py
@@ -1,7 +1,7 @@
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "postgresql+asyncpg://ByteMe:root@localhost:5432/ByteMe"
+DATABASE_URL = "postgresql+asyncpg://byteme:root@localhost:5432/ByteMe"
 
 engine = create_async_engine(DATABASE_URL, echo=True)
 SessionLocal = sessionmaker(bind=engine, class_=AsyncSession, expire_on_commit=False)
